2da18d69fb6da48db79ec27ce41083ed3d98fed0
Gerald <102348985+geraldngjx@users.noreply.github.com>
2022-07-10 01:46:12 +0800

    updated clusterMap javascript

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
-    style: 'mapbox://styles/mapbox/light-v10',
+    style: 'mapbox://styles/mapbox/outdoors-v11',
     center: [ 103.808052586332, 1.3516161224392 ],
     zoom: 10.5
 });
